a0fd53cb641a7f6a709792f78beed6294099ffd5
Nagesh Borate <nagesh.borate@oracle.com>
2020-04-14 23:14:52 +0530

    added new functions

--- src/AppData.js
+++ src/AppData.js
@@ -20,6 +20,8 @@ class AppData
 
         this.lastPlayedCard = 32;
 
+        this.discardPile = [];
+
         this.getCardSuit = function(cardIdx)
         {
             return this.deck[cardIdx][0]
@@ -39,7 +41,101 @@ class AppData
         {
             return this.deck[this.lastPlayedCard]
         }.bind(this);
+
+        this.addPlayer = function(player)
+        {
+            if (this.players.indexOf(player) < 0)
+                this.players.push(player);
+        }.bind(this);
+
+        this.generateDiscardPile = function()
+        {
+            let discardPile = [];
+            for (let idx = 0 ; idx < 52 ; idx++)
+            {
+                let cardIdx;
+                while (true)
+                {
+                    cardIdx = getRandomIntInclusive(0, 51);
+                    if (isNumberAlreadyGenerated(cardIdx, discardPile))
+                        continue;
+                    else
+                        break;
+                }
+                discardPile.push(cardIdx);
+            }
+
+            this.discardPile = discardPile;
+        }.bind(this);
+
+        this.generatePlayerCards = function(count = 5)
+        {
+            return this.discardPile.splice(0, count);
+        }.bind(this);
+
+        this.generateLastPlayedCard = function()
+        {
+            return this.discardPile.shift();
+        }.bind(this);
+
+        this.startNewGame = function()
+        {
+            if (this.players && this.players.length > 0)
+            {
+                this.generateDiscardPile();
+                this.playerCards = {};
+
+                for (let idx = 0 ; idx < this.players.length ; idx++)
+                {
+                    this.playerCards[idx] = this.generatePlayerCards();
+                }
+
+                this.lastPlayedCard = this.generateLastPlayedCard();
+            }
+        }.bind(this);
+
+        this.getPlayerCardsWithValidity = function(player)
+        {
+            if (this.players.indexOf(player) >= 0)
+            {
+                let returnArray = [];
+                let lastPlayedCardSuit = this.getCardSuit(this.lastPlayedCard);
+                let lastPlayedCardValue = this.getCardValue(this.lastPlayedCard);
+
+                let playerIdx = this.players.indexOf(player);
+                let playerCards = this.playerCards[playerIdx];
+                for (let idx = 0 ; idx < playerCards.length ; idx++)
+                {
+                    let playerCardIdx = playerCards[idx];
+                    let playerCardSuit = this.getCardSuit(playerCardIdx);
+                    let playerCardValue = this.getCardValue(playerCardIdx);
+                    let isPlayerCardValue8 = this.getCardValue(playerCardIdx) === '8';
+
+                    returnArray.push(
+                    {
+                        cardIdx: playerCardIdx,
+                        card: this.getCard(playerCardIdx),
+                        valid: isPlayerCardValue8 || (lastPlayedCardSuit === playerCardSuit) || (lastPlayedCardValue === playerCardValue) || false;
+                    });
+                }
+                return returnArray;
+            }
+        }
     }
 }
+
+let getRandomIntInclusive = function(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+let isNumberAlreadyGenerated = function(generatedNumber, generatedNumbers)
+{
+    let index = generatedNumbers.indexOf(generatedNumber);
+
+    return (index >= 0);
+};
 
 export default new AppData();
